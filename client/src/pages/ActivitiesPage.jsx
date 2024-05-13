43528cd0c023679d72e2107ec0fef26a7a2874da
Richard Lee <richardzhyulee@gmail.com>
2024-05-12 20:08:49 -0700

    Set up Socket.IO

--- client/src/pages/ActivitiesPage.jsx
+++ client/src/pages/ActivitiesPage.jsx
@@ -2,12 +2,22 @@ import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import io from 'socket.io-client';
 import SERVER_URL from '../constants/constants'
-const socket = io('ws://localhost:8080');
+const socket = io('http://localhost:8080');
 
 const ActivitiesPage = () => {
 
     useEffect(() => {
+        socket.on('connect', () => {
+            console.log('Connected to server');
+        });
 
+        socket.on('disconnect', () => {
+            console.log('Disconnected from server');
+        });
+
+        return () => {
+            socket.disconnect();
+        }
     }, []);
 
     return (
--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 
 const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
 const mongoose = require('mongoose');
-const cors = require('cors');
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+});
+
 const authRoutes = require('./routes/authRoutes');
 const apiRoutes = require('./routes/apiRoutes');
-const port = process.env.PORT || 8080;
 const passport = require('passport');
+const cors = require('cors');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const store = new MongoDBStore({
@@ -36,7 +46,6 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
 
@@ -46,13 +55,21 @@ app.get('/', (req, res) => {
     });
 });
 
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+})
+
+const port = process.env.PORT || 8080;
 mongoose.connect(process.env.DATABASE_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     console.log('Connected to MongoDB');
-    app.listen(port, () => {
-        console.log(`App started on port ${port}`);
+    server.listen(port, () => {
+        console.log(`Server started on port ${port}`);
     });
 }).catch((error) => {
     console.log(error);
